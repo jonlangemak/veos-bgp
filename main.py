1a283e3057fcb5255cb4af85d6c6fe10849aecad
Jon Langemak <jon@interubernet.com>
2015-10-21 15:00:46 -0500

    First push

---
+++ main.py
@@ -0,0 +1,47 @@
+
+from jsonrpclib import Server
+import ssl
+import dns
+import json
+from prettytable import PrettyTable 
+
+from dns import resolver
+from dns import reversename
+
+res = dns.resolver.Resolver()
+res.nameservers = ['10.20.30.13']
+
+
+# Deal with self signed certificate
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
+switch = Server("https://vagrant:vagrant@localhost:8443/command-api")
+response = switch.runCmds(1, ["show ip bgp summary"])
+
+print json.dumps(response, indent=4, sort_keys=True)
+
+
+table = PrettyTable(["Peer", "DNS Name", "ASN"])
+
+for peers in response[0]['vrfs']['default']['peers']:
+    try:
+        addr = reversename.from_address(peers)
+        resolved = res.query(addr, "PTR")[0]
+    except dns.resolver.NXDOMAIN:
+        resolved = "No DNS record"
+
+    asn = response[0]['vrfs']['default']['peers'][peers]['asn']
+
+    table.add_row([peers, resolved, asn])
+
+
+
+print table
